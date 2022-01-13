d8194a32ae3a40e6c6c26a2b01032286dc7b5c8f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-01-12 22:52:18 -0400

    taking in, and working on input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hey!\nPlease enter your first name: ");
+        String name = in.next();
+        System.out.println("Hey: " + name);
     }
 }

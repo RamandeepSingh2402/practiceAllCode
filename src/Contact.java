d12493dd7153ac88a7f5c486d5c85beb95d45fce
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-01-17 16:02:28 -0400

    Added feature of contact

---
+++ src/Contact.java
@@ -0,0 +1,9 @@
+public class Contact {
+    public String contactName;
+    public int contactNumber;
+    public Contact(String contactName, int contactNumber){
+        this.contactNumber = contactNumber;
+        this.contactName = contactName;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,32 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Hey!\nPlease enter your first name: ");
         String name = in.next();
-        String name1 = "";
+        in.nextLine();
+        System.out.print("Please enter your contact number as well: ");
+        int contactNum = in.nextInt();
         LinkedList<String> names = new LinkedList<String>();
         names.add(name);
         System.out.println("Hey: " + names.get(0));
-        System.out.println("I'm glad to see you!");
-        System.out.println("So as anticipated, how can I help you today?");
-        in.nextLine();
-        String record = in.nextLine();
-        in.nextLine();
-        int count = 1;
-        File firstVisit = new File("firstVisit.txt");
-        FileWriter writer = new FileWriter("firstVisit.txt");
-
-        while(!record.equalsIgnoreCase("done")) {
-                writer.write(record);
-                writer.close();
-                record = in.nextLine();
-            }
-        }
+        System.out.println("I'm glad to see you!\nI have successfully created your contact!");
+//        System.out.println("So as anticipated, how can I help you today?");
+        //in.nextLine();
+//        String record = in.nextLine();
+//        in.nextLine();
+//        int count = 1;
+//        File firstVisit = new File("firstVisit.txt");
+//        FileWriter writer = new FileWriter("firstVisit.txt");
+//
+//        while(!record.equalsIgnoreCase("done")) {
+//                writer.write(record);
+//                writer.close();
+//                record = in.nextLine();
+//            }
+        Contact contact = new Contact(name, contactNum);
+        LinkedList<Contact> namesOfContacts = new LinkedList<>();
+        namesOfContacts.add(contact);
+        System.out.print("First contact's name is: ");
+        System.out.println(namesOfContacts.getFirst().contactName);
 
     }
+
+}

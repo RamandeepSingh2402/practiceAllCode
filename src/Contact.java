9848e4bd63ea22b4f295ab8c15b95f3582204e6e
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-08-23 09:43:45 -0300

    making the old program work

--- src/Contact.java
+++ src/Contact.java
@@ -1,18 +1,9 @@
 public class Contact {
     public String contactName;
     public int contactNumber;
-<<<<<<< HEAD
 
     public Contact(String contactName, int contactNumber) {
         this.contactNumber = contactNumber;
         this.contactName = contactName;
     }
 }
-=======
-    public Contact(String contactName, int contactNumber){
-        this.contactNumber = contactNumber;
-        this.contactName = contactName;
-    }
-}
-
->>>>>>> 9a90c7a48228119461ac882b9c4b77498a9a8f9f

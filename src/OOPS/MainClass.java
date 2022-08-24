b846b92dd2d0f18e521d2feb826cd3317eab7f0a
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-08-24 11:24:58 -0300

    revised the concept of Constructors

--- src/OOPS/MainClass.java
+++ src/OOPS/MainClass.java
@@ -3,16 +3,23 @@ package OOPS;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //creating first person
         Person p1 = new Person();
         p1.age = 24;
         p1.name = "Anuj";
+
+        //printing the details of the first object
         System.out.println(p1.age + " " + p1.name);
 
+        //creating the second object of the Person class
         Person p2 = new Person();
         p2.name = "Shivam";
         p2.age = 25;
+
+        //printing the details of the second person
         System.out.println(p2.age + " " + p2.name);
 
+        //calling different methods of the existing persons
         p1.eat();
         p2.walk();
         p2.walk(2);
@@ -22,6 +29,10 @@ public class MainClass {
 class Person {
     String name;
     int age;
+
+    public Person(){
+        System.out.println("Creating an object.");
+    }
     void walk(){
         System.out.println( name + " is walking.");
     }

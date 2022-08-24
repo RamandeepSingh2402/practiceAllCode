9859d651dfd5ebd9cb72c6a4b6f72babbcaeaad1
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-08-24 14:08:48 -0300

    public access modifier

--- src/OOPS/MainClass.java
+++ src/OOPS/MainClass.java
@@ -1,34 +1,39 @@
 package OOPS;
 
+import encapsulation.EncapsulationIntro;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        //creating first person
-        Person p1 = new Person();
-        p1.age = 24;
-        p1.name = "Anuj";
-
-        //printing the details of the first object
-        System.out.println(p1.age + " " + p1.name);
-
-        //creating the second object of the Person class
-        Person p2 = new Person(31, "Shivam Sharma");
-
-        //printing the details of the second person
-        System.out.println(p2.age + " " + p2.name);
-
-        //calling different methods of the existing persons
-        p1.eat();
-        p2.walk();
-        p2.walk(2);
-
-        // creating a new developer
-        Developer d1 = new Developer(20, "Ramandeep");
-        d1.walk();
-
-        //calling "count" which is the property of the Person class now instead of the object
-        System.out.println(Person.count);
-    }
+        EncapsulationIntro obj = new EncapsulationIntro();
+        obj.doWork();
+//
+//        //creating first person
+//        Person p1 = new Person();
+//        p1.age = 24;
+//        p1.name = "Anuj";
+//
+//        //printing the details of the first object
+//        System.out.println(p1.age + " " + p1.name);
+//
+//        //creating the second object of the Person class
+//        Person p2 = new Person(31, "Shivam Sharma");
+//
+//        //printing the details of the second person
+//        System.out.println(p2.age + " " + p2.name);
+//
+//        //calling different methods of the existing persons
+//        p1.eat();
+//        p2.walk();
+//        p2.walk(2);
+//
+//        // creating a new developer
+//        Developer d1 = new Developer(20, "Ramandeep");
+//        d1.walk();
+//
+//        //calling "count" which is the property of the Person class now instead of the object
+//        System.out.println(Person.count);
+//
+      }
 }
 
 class Developer extends Person{
